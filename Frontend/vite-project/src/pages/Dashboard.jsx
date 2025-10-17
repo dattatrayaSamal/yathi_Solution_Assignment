37369bd88d114ee2c6a7a53381a45e864c71c0ef
Dattatraya Samal <82104415+dattatrayaSamal@users.noreply.github.com>
2025-10-17 17:25:37 +0530

    update

--- Frontend/vite-project/src/pages/Dashboard.jsx
+++ Frontend/vite-project/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import MealCard from "../components/MealCard";
+import Cart from "../components/Cart";
 
 const Dashboard = () => {
   const [meals, setMeal] = useState([]);
@@ -22,7 +23,12 @@ const Dashboard = () => {
     const total = cart.reduce((sum, item) => sum + item.price, 0);
     const response = await fetch("http://localhost:8000/orders", {
       method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: cart, total }),
     });
+    const data = await response.json();
+    alert(data.message);
+    setCart([]);
   };
   return (
     <div>
@@ -32,6 +38,8 @@ const Dashboard = () => {
           <MealCard key={meal._id} meal={meal} addToCart={addToCart} />
         ))}
       </div>
+      <Cart cart={cart} placeOrder={placeOrder} />
     </div>
   );
 };
+export default Dashboard;
